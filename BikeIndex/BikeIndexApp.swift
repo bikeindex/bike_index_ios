fd562c1fcf6c964898e1b024c91968c87ab35a1d
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2023-11-18 19:14:24 -0500

    Initial Commit

---
+++ BikeIndex/BikeIndexApp.swift
@@ -0,0 +1,32 @@
+//
+//  BikeIndexApp.swift
+//  BikeIndex
+//
+//  Created by Jack on 11/18/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BikeIndexApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
