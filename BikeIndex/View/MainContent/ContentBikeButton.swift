86f451eaad60324c3cfc97950876f226ba938a95
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2024-01-13 21:15:02 -0500

    Improve ContentBikeButton espeically for images, add sample data to preview

--- BikeIndex/View/MainContent/ContentBikeButton.swift
+++ BikeIndex/View/MainContent/ContentBikeButton.swift
@@ -17,43 +17,85 @@ struct ContentBikeButtonView: View {
         }, label: {
             VStack {
                 ZStack {
-                    if let hero = bike.largeImage {
-                        AsyncImage(url: hero) { image in
-                            image.resizable().scaledToFill()
-                        } placeholder: {
-                            Image(systemName: "bicycle.circle.fill")
-                        }
+                    AsyncImage(url: bike.largeImage) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    } placeholder: {
+                        Image(systemName: "bicycle.circle.fill")
+                            .resizable()
                     }
-                    Text(bike.title)
-                        .font(.largeTitle)
                 }
-                .scaledToFit()
+                .frame(width: 200, height: 200)
                 .clipShape(RoundedRectangle(cornerRadius: 24))
 
-                Text("\(String(bike.year ?? 0)) \(bike.manufacturerName)")
+                Text(bike.title)
             }
         })
     }
 }
 
 #Preview {
-    let sampleBike = Bike(identifier: 1,
+    let sampleBike1 = Bike(identifier: 1,
                           primaryColor: FrameColor.bareMetal,
                           manufacturerName: "Jamis",
                           typeOfCycle: BicycleType.bike,
                           status: .withOwner,
                           stolenCoordinateLatitude: 0,
                           stolenCoordinateLongitude: 0,
-                          largeImage: URL(string: "https://placekitten.com/200/200").unsafelyUnwrapped,
+                          largeImage: URL(string: "https://placekitten.com/200/200"),
                           url: URL(string: "about:blank").unsafelyUnwrapped,
                           publicImages: [
                             "https://placekitten.com/200/200",
                           ])
 
+    let sampleBike2 = Bike(identifier: 2,
+                          primaryColor: FrameColor.bareMetal,
+                          manufacturerName: "Wide",
+                          typeOfCycle: BicycleType.bike,
+                          status: .withOwner,
+                          stolenCoordinateLatitude: 0,
+                          stolenCoordinateLongitude: 0,
+                          largeImage: URL(string: "https://placekitten.com/500/200"),
+                          url: URL(string: "about:blank").unsafelyUnwrapped,
+                          publicImages: [
+                            "https://placekitten.com/200/200",
+                          ])
+
+    let sampleBike3 = Bike(identifier: 3,
+                          primaryColor: FrameColor.bareMetal,
+                          manufacturerName: "Tall",
+                          typeOfCycle: BicycleType.bike,
+                          status: .withOwner,
+                          stolenCoordinateLatitude: 0,
+                          stolenCoordinateLongitude: 0,
+                          largeImage: URL(string: "https://placekitten.com/200/500"),
+                          url: URL(string: "about:blank").unsafelyUnwrapped,
+                          publicImages: [
+                            "https://placekitten.com/200/200",
+                          ])
+
+    let sampleBike4 = Bike(identifier: 4,
+                          primaryColor: FrameColor.bareMetal,
+                          manufacturerName: "Empty",
+                          typeOfCycle: BicycleType.bike,
+                          status: .withOwner,
+                          stolenCoordinateLatitude: 0,
+                          stolenCoordinateLongitude: 0,
+                          largeImage: nil,
+                          url: URL(string: "about:blank").unsafelyUnwrapped,
+                          publicImages: [
+                            "https://placekitten.com/200/200",
+                          ])
+
+    let samples = [sampleBike1, sampleBike2, sampleBike3, sampleBike4]
+
     return ScrollView {
         ProportionalLazyVGrid {
-            ContentBikeButtonView(path: .constant(NavigationPath()),
-                                  bike: sampleBike)
+            ForEach(samples) { bike in
+                ContentBikeButtonView(path: .constant(NavigationPath()),
+                                      bike: bike)
+            }
         }
     }
 }
