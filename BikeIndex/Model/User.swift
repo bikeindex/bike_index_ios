9570c3d2aea1de25a2540b5386029d10a43497a6
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2024-10-06 17:47:33 -0400

    Fix Xcode 16 warnings (#3)
    # Description
    
    - Fix warnings from project built with Xcode Version 16.0 (16A242d).
    - Clean up some minor comments
    - Note: these changes **do not** correct Swift 6 Strict Concurrency Checking warnings. That will be addressed separately.

--- BikeIndex/Model/User.swift
+++ BikeIndex/Model/User.swift
@@ -10,7 +10,7 @@ import SwiftData
 import OSLog
 
 @Model final class AuthenticatedUser: BikeIndexIdentifiable, CustomDebugStringConvertible {
-    // TODO: Check if this can be Int
+    // TODO: Check if `identifier` can be Int
     @Attribute(.unique) private(set) var identifier: String
     @Relationship(deleteRule: .cascade) var user: User?
 
@@ -31,7 +31,7 @@ import OSLog
     }
 }
 
-@Model final class User { // CustomDebugStringConvertible {
+@Model final class User {
     @Attribute(.unique) fileprivate(set) var email: String
     fileprivate(set) var username: String
     fileprivate(set) var name: String
@@ -55,11 +55,11 @@ import OSLog
 }
 
 @Model final class Organization {
-    @Attribute(.unique) let identifier: Int
-    let name: String
-    let slug: String
-    let accessToken: Token
-    let userIsOrganizationAdmin: Bool
+    @Attribute(.unique) var identifier: Int
+    var name: String
+    var slug: String
+    var accessToken: Token
+    var userIsOrganizationAdmin: Bool
 
     //    @Relationship(deleteRule: .cascade, inverse: \AuthenticatedUser.memberships)
     //    var authorizedUsers: [AuthenticatedUser]? = []
