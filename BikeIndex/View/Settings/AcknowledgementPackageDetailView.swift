b3060a303480376b383fe95e7011573d83ba01d7
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2024-12-07 13:52:45 -0500

    Redo Acknowledgements web view navigation structure to fix blank page on iOS 18

--- BikeIndex/View/Settings/AcknowledgementPackageDetailView.swift
+++ BikeIndex/View/Settings/AcknowledgementPackageDetailView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct AcknowledgementPackageDetailView: View {
     let package: AcknowledgementPackage
-    @Binding var url: URL?
+    @Binding var showRepositoryUrl: Bool
 
     var body: some View {
         Text(package.fullLicense())
             .toolbar {
                 ToolbarItemGroup(placement: .topBarTrailing) {
                     Button(action: {
-                        url = package.repository
+                        showRepositoryUrl = true
                     }, label: {
                         Label("Open Repository", systemImage: "link")
                     })
@@ -30,13 +30,13 @@ struct AcknowledgementPackageDetailView: View {
     let package = AcknowledgementPackage(title: "BikeIndex iOS",
                                          license: .gnuAfferoGPLv3,
                                          copyright: "2023 © Bike Index, a 501(c)(3) nonprofit - EIN 81-4296194",
-                                         repository: URL(string: "https://github.com/bikeindex/bike_index_ios"))
-    let binding: Binding<URL?> = Binding {
-        package.repository
+                                         repository: URL(string: "https://github.com/bikeindex/bike_index_ios")!)
+    let showUrl: Binding<Bool> = Binding {
+        false
     } set: { _ in }
 
     return NavigationStack {
-        AcknowledgementPackageDetailView(package: package, url: binding)
+        AcknowledgementPackageDetailView(package: package, showRepositoryUrl: showUrl)
         .navigationTitle("BikeIndex iOS")
         .navigationBarTitleDisplayMode(.inline)
     }
