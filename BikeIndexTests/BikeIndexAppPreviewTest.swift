10c4221e583edff4f90be2cec51db65be19d7544
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2024-12-27 14:29:58 -0500

    Tests: Add BikeIndexAppPreviewTest to leverage Emerge Tools SnapshotPreview package

---
+++ BikeIndexTests/BikeIndexAppPreviewTest.swift
@@ -0,0 +1,22 @@
+//
+//  BikeIndexAppPreviewTest.swift
+//  BikeIndexTests
+//
+//  Created by Jack on 12/27/24.
+//
+
+import XCTest
+import SnapshottingTests
+
+class BikeIndexAppPreviewTest: SnapshotTest {
+
+    // Return the type names of previews like "MyApp.MyView._Previews" to selectively render only some previews
+    func snapshotPreviews() -> [String]? {
+        return nil
+    }
+
+    // Use this to exclude some previews from generating
+    func excludedSnapshotPreviews() -> [String]? {
+        return nil
+    }
+}
