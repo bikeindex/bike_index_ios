0dd2093e513defd7bdb887d1484e288a8dc98026
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2025-03-09 00:03:19 -0500

    Fix: Add BikeRegistration.serial == unknown unit test
    - Migrate BikeRegistrationTests to new Swift Testing framework

--- UnitTests/BikeRegistrationTests.swift
+++ UnitTests/BikeRegistrationTests.swift
@@ -5,16 +5,16 @@
 //  Created by Jack on 12/28/23.
 //
 
-import XCTest
+import Foundation
+import Testing
 
 @testable import BikeIndex
 
-final class BikeRegistrationTests: XCTestCase {
-
+struct BikeRegistrationTest {
     /// Ensure that the BikeRegistration model uses lower-case values for the frame colors.
     /// All other frame color usages must be title-case.
-    func test_bike_registration_lowercase_color() throws {
-        let rawJsonData = try XCTUnwrap(MockData.sampleBikeJson.data(using: .utf8))
+    @Test func test_lowercase_color() throws {
+        let rawJsonData = try #require(MockData.sampleBikeJson.data(using: .utf8))
         let output = try JSONDecoder().decode(BikeResponse.self, from: rawJsonData)
         let bike = output.modelInstance()
 
@@ -26,12 +26,21 @@ final class BikeRegistrationTests: XCTestCase {
             propulsion: nil,
             ownerEmail: "")
 
-        XCTAssertEqual(registrationModel.primary_frame_color, "green")
-        XCTAssertEqual(registrationModel.secondary_frame_color, "blue")
-        XCTAssertEqual(registrationModel.tertiary_frame_color, "stickers tape or other cover-up")
+        #expect(registrationModel.primary_frame_color == "green")
+        #expect(registrationModel.secondary_frame_color == "blue")
+        #expect(registrationModel.tertiary_frame_color == "stickers tape or other cover-up")
 
-        XCTAssertEqual(registrationModel.cycle_type_name, .bike)
-        XCTAssertNil(registrationModel.propulsion)
+        #expect(registrationModel.cycle_type_name == .bike)
+        #expect(registrationModel.propulsion == nil)
     }
 
+    /// Ensure that Bike -> BikeRegistration -> `serial` data flow is correct
+    @Test func test_unknown_serial() {
+        let unknownSerialBike = Bike()
+
+        let registrationModel = BikeRegistration(
+            bike: unknownSerialBike, mode: .myOwnBike, stolen: nil, propulsion: nil,
+            ownerEmail: "tester@bikeindex.org")
+        #expect(registrationModel.serial == "unknown")
+    }
 }
