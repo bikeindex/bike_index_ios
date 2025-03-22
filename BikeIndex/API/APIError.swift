9bb21d33186ff6bb03beb96735856a0ecffd2ae1
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2025-03-22 08:08:26 -0400

    Fix: Add APIError: LocalizedError conformance, parse and display error (#42)
    # Description
    
    - Fix typos
    - Update API.post inline documentation
    - Move APIError to its own file
    - Add APIErrorMessage type to decode {"error":""} responses when
    available
    
    ### Screenshots
    
    | Before | After |
    | -- | -- |
    | ![Simulator Screenshot - iPhone 16 Plus - 2025-03-22 at 01 44
    22](https://github.com/user-attachments/assets/880fd52b-f53f-440c-af6c-4c7f9a75df5b)
    | ![Simulator Screenshot - iPhone 16 Plus - 2025-03-22 at 01 42
    42](https://github.com/user-attachments/assets/a9efece9-edf6-4113-a709-2bb3d06f5a2b)
    |

---
+++ BikeIndex/API/APIError.swift
@@ -0,0 +1,34 @@
+//
+//  APIError.swift
+//  BikeIndex
+//
+//  Created by Jack on 3/22/25.
+//
+
+import Foundation
+
+struct APIErrorMessage: Decodable {
+    var error: String
+}
+
+enum APIError: Error, LocalizedError {
+    case cacheHit
+    case clientError(code: Int, data: Data?)
+    case postMissingContents(endpoint: APIEndpoint)
+
+    var errorDescription: String? {
+        switch self {
+        case .cacheHit:
+            "Cache Hit"
+        case .clientError(let code, let data):
+            // Decode JSON of format: `{ "error": "" }`
+            if let data, let message = try? JSONDecoder().decode(APIErrorMessage.self, from: data) {
+                "\(code) \(message.error)"
+            } else {
+                "\(code) - Failed to decode error message."
+            }
+        case .postMissingContents(let endpoint):
+            "Could not POST, missing contents for \(endpoint)"
+        }
+    }
+}
