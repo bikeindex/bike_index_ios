108cd899141b6ffb222ade8c9babaf9b7998d5ef
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2025-04-28 10:27:09 -0400

    Feat: group Bike sections by manufacturer (#62)
    # Description
    
    - Add Menu to change main content page grouping of bikes by either A)
    status or B) manufacturer name
    - Grouping selection is persisted in user defaults
    - Remove unused BikeIndexIdentifiable, replace with Bike.BikeIdentifier,
    and change BikeDetailView to query for its Bike by API/Rails identifier
    authoritative ID.
    - Add BikesList to contain BikesSection children views.
    - Rename BikesStatusSection to general BikesSection and refine behavior
    for generalizing.
    - Move MainContentPage state fields to MainContentPage.ViewModel.
    - Add loading indicator to toolbar (topBarTrailing)
    - Update documentation comments
    
    ### Screenshot
    
    <img alt="Screenshot 2025-04-27 at 00 14 56"
    src="https://github.com/user-attachments/assets/d8567ccc-830e-471f-82e6-14076dcee03f"
    width="50%">

---
+++ BikeIndex/View/MainContent/BikesList.swift
@@ -0,0 +1,60 @@
+//
+//  BikesList.swift
+//  BikeIndex
+//
+//  Created by Jack on 3/23/25.
+//
+
+import SectionedQuery
+import SwiftData
+import SwiftUI
+
+/// Display multiple sections of bikes together
+struct BikesList: View {
+    @Binding var path: NavigationPath
+
+    @SectionedQuery(\Bike.statusString)
+    var bikes: SectionedResults<String, Bike>
+
+    var group: MainContentPage.ViewModel.GroupMode
+
+    init(path: Binding<NavigationPath>, group: MainContentPage.ViewModel.GroupMode) {
+        _path = path
+        self.group = group
+        _bikes = group.sectionQuery
+    }
+
+    var body: some View {
+        if bikes.isEmpty {
+            ContentUnavailableView("No bikes registered", systemImage: "bicycle.circle")
+                .padding()
+        } else {
+            ProportionalLazyVGrid(pinnedViews: [.sectionHeaders]) {
+                ForEach(bikes) { section in
+                    /// Map the broad group (byStatus, byManufacturer)
+                    /// to a _specific_ status or manufacturer to display in _this_ section.
+                    /// Use an optional SectionValue because the ``BikeStatus`` has to map
+                    /// from a string (we can search on ``Bike/statusString`` but **not** Bike.status
+                    /// so there could be a BikeStatus(rawValue:) initializer that fails.
+                    let section: BikesSection.SectionValue? =
+                        switch group {
+                        case .byStatus:
+                            if let status = BikeStatus(rawValue: section.id) {
+                                BikesSection.SectionValue.byStatus(status)
+                            } else {
+                                BikesSection.SectionValue?(nil)
+                            }
+                        case .byManufacturer: .byManufacturer(section.id)
+                        }
+                    if let section {
+                        BikesSection(
+                            path: $path,
+                            section: section)
+                    } else {
+                        Text("Error rendering section")
+                    }
+                }
+            }
+        }
+    }
+}
