523b2903a8220236628a96f8c5ba627b91ebf5e4
Milo Wyner <hello@milowyner.com>
2025-06-27 16:44:58 -0700

    Revert "Temporarily rename universal links test"
    This reverts commit 6814a3c49dc562555c8a3bf8994e62c92294e3f1.

--- UITests/UniversalLinksUITest.swift
+++ UITests/UniversalLinksUITest.swift
@@ -14,7 +14,7 @@ let stickerUrl = URL(string: "bikeindex://https://bikeindex.org/bikes/scanned/A4
 extension BikeIndexUITests {
 
     /// Sometimes this fails when a page plainly fails to load, may need to add more resiliency.
-    func test_ci_fail() throws {
+    func test_authenticated_bikes_scanned_id_universal_link() throws {
         XCTAssert(false)
 //        try UniversalLinksRobot(app: app, testCase: self)
 //            .startWithSignIn()
