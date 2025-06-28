916646cc2b26c2671f4aae02969b690456b476c3
Milo Wyner <hello@milowyner.com>
2025-06-27 20:14:34 -0700

    Feat: Adopt robot pattern for universal links UI test (#85)
    # Description
    
    - Previously the universal links UI test
    `BikeIndexUITests.test_authenticated_bikes_scanned_id_universal_link()`
    was succeeding even though it was timing out because the queries used to
    check for existence had differences
    - Add asserts to properly check for their existence
    - Implement the first stage of adopting the Robot Pattern for UI testing
    by applying it to the universal links UI test
    
    ## Screenshots
    
    | Previous code (after adding existence asserts) | New code |
    | --- | --- |
    | <img width="777" alt="Screenshot 2025-06-21 at 10 42 04 AM"
    src="https://github.com/user-attachments/assets/1aee2432-88c3-425a-90a1-c8736e2687cb"
    /> | <img width="788" alt="Screenshot 2025-06-21 at 2 11 09 PM"
    src="https://github.com/user-attachments/assets/8b9334da-7497-4339-bb98-ac1056557c1e"
    /> |

---
+++ UITests/UniversalLinksRobot.swift
@@ -0,0 +1,41 @@
+//
+//  UniversalLinksRobot.swift
+//  BikeIndex
+//
+//  Created by Milo Wyner on 6/21/25.
+//
+
+import XCUIAutomation
+
+final class UniversalLinksRobot: Robot {
+    private lazy var stickerHeader = app.navigationBars.staticTexts["A 403 40"]
+    private lazy var unlinkedMessage: [XCUIElement] = [
+        app.webViews.staticTexts["You scanned the sticker"],
+        app.webViews.staticTexts["A 403 40"],
+        app.webViews.staticTexts[", which is assigned to this bike."]
+    ]
+
+    @discardableResult
+    func openLink() -> Self {
+        // NOTE: Deeplinks will remove the second `:` from `bikeindex://https://bikeindex...`
+        XCUIDevice.shared.system.open(stickerUrl)
+
+        return self
+    }
+
+    @discardableResult
+    func checkStickerHeader() -> Self {
+        assert(stickerHeader, [.exists])
+
+        return self
+    }
+
+    @discardableResult
+    func checkUnlinkedMessage() -> Self {
+        for message in unlinkedMessage {
+            assert(message, [.exists])
+        }
+
+        return self
+    }
+}
