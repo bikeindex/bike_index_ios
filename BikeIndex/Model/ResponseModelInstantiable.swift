94700492c951ba551f3e57ab0c24dc93c93cfdb9
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2025-08-09 16:56:15 -0400

    Move ResponseModelInstantiable to its own file

---
+++ BikeIndex/Model/ResponseModelInstantiable.swift
@@ -0,0 +1,15 @@
+//
+//  into.swift
+//  BikeIndex
+//
+//  Created by Jack on 8/1/25.
+//
+
+import Foundation
+
+/// Convert a network response from a Decodable struct into its corresponding @Model class instance.
+protocol ResponseModelInstantiable: Decodable {
+    associatedtype ModelInstance
+
+    func modelInstance() -> ModelInstance
+}
