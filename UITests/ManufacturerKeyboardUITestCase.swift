7653c47e5113a7ec2605ea5d3b1e3285814ec8cb
Milo Wyner <hello@milowyner.com>
2025-08-19 13:20:24 -0700

    Tests: Adopt Robot pattern for test_ManufacturerEntryView_keyboard_control

--- UITests/ManufacturerKeyboardUITestCase.swift
+++ UITests/ManufacturerKeyboardUITestCase.swift
@@ -22,20 +22,14 @@ final class ManufacturerKeyboardUITestCase: XCTestCase {
     }
 
     func test_ManufacturerEntryView_keyboard_control() throws {
-        app.launch()
-        try signIn(app: app)
-
-        let registerABike = app.buttons["Register a bike"]
-        _ = registerABike.waitForExistence(timeout: timeout)
-        registerABike.tap()
-
-        let manufacturerEntryView = app.textFields["manufacturerSearchTextField"]
-        _ = manufacturerEntryView.waitForExistence(timeout: timeout)
-        manufacturerEntryView.tap()
-
-        manufacturerEntryView.typeText("J")
-        manufacturerEntryView.typeText("a")
-        manufacturerEntryView.typeText("m")
-        manufacturerEntryView.typeText("is")
+        try MainContentRobot(app)
+            .startWithSignIn()
+            .tapRegisterBikeButton()
+            .tapManufacturerTextField()
+            .typeIntoManufacturerTextField("J")
+            .typeIntoManufacturerTextField("a")
+            .typeIntoManufacturerTextField("m")
+            .typeIntoManufacturerTextField("is")
+            .checkManufacturerTextFieldContains(text: "Jamis")
     }
 }
