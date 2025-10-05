1fc36c18e1c9514dc7f0091f8e589c190548741a
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2025-10-05 02:55:25 -0400

    Feat: Update app icons for iOS 26 (#108)
    # Description
    
    - Update icons for iOS 26 Liquid Glass styles using Icon Composer
        - iOS 17 and 18 devices will see the new Liquid Glass style icons too (unless more effort is taking which could collide with the Assets.xcassets for the alternate icon picker)
    - Increase the scale of SVGs to make alignment within Icon Composer easier. 
        - Sadly this increases file size but having each separate Icon Composer file with slightly different offsets and scale was unwieldy. 
    - Change the default icon to the black shield on white background to match Light / Dark / Mono (tint) / Glass modes more directly
    - Icons are aligned to A) the top line of the inner arc above the text is at 13% from the top and B) the bottom line of the inner arc below the text is at 93% from the top
    
    Preview of icons available at: https://github.com/bikeindex/bike_index_ios/blob/main/docs/app-icons.md
    
    ### Guides
    
    - https://developer.apple.com/documentation/Xcode/creating-your-app-icon-using-icon-composer
    - https://developer.apple.com/icon-composer/
    - https://mjtsai.com/blog/2025/06/23/icon-composer-notes/
    
    ### Screenshots
    
    | Light | Dark |
    | --- | --- |
    | <img width="1206" height="2622" alt="Screenshot 2025-10-04 at 19 07 25" src="https://github.com/user-attachments/assets/aa4d6e15-cdc9-4182-9148-15ddefeed5b6" /> | <img width="1206" height="2622" alt="Screenshot 2025-10-04 at 19 07 15" src="https://github.com/user-attachments/assets/37799edb-d53e-4444-afa1-84db5a416d75" /> |

---
+++ BikeIndex/Model/IconPicker/AppIcon.swift
@@ -0,0 +1,81 @@
+//
+//  AppIcon.swift
+//  BikeIndex
+//
+//  Created by Jack on 11/19/23.
+//
+
+import SwiftUI
+import UIKit
+
+/// Represents preview icons from asset catalog for display in-app.
+/// These asset catalog resources must append "-in-app" to load the resource.
+/// Actual icons are in Icon Composer format for iOS 26.
+enum AppIcon: String, CaseIterable, Identifiable {
+    case primary = "AppIcon"
+    case shadow = "Shadow"
+    case grayscale = "Grayscale"
+    case striped = "Striped"
+    case pride = "Pride"
+
+    /// From iOS 18+ icon 'previews' must be in a regular image asset catalog, thus "-in-app".
+    /// See Assets.xcassets
+    var id: String { rawValue + "-in-app" }
+
+    /// Provide a name suitable for the Alternate App Icon API to ingest.
+    /// This is not suitable for displaying a UIImage in-app
+    var iconName: String? {
+        switch self {
+        case .primary:
+            return nil
+        default:
+            return rawValue
+        }
+    }
+
+    /// Provide a UIImage suitable for display in-app
+    var image: UIImage {
+        if let uiImage = UIImage(named: id) {
+            return uiImage
+        } else {
+            // Fallback SF Symbol when an AppIcon cannot be loaded
+            return UIImage(systemName: "questionmark.app.dashed")!
+        }
+    }
+
+    /// User-facing description for each icon.
+    var description: String {
+        switch self {
+        case .primary:
+            return "Bike Index"
+        case .grayscale:
+            return "Grayscale"
+        case .shadow:
+            return "Shadow"
+        case .striped:
+            return "Striped"
+        case .pride:
+            return "Pride 🏳️‍🌈"
+        }
+    }
+}
+
+// Validates Views with UIImages (without using models)
+#Preview("App Icon Direct UIImage Access") {
+    VStack {
+        Form {
+            Section {
+                Image(uiImage: UIImage(named: "AppIcon-in-app")!)
+                    .appIcon(scale: .large)
+
+                Label(
+                    title: { Text("App Icon") },
+                    icon: {
+                        Image(uiImage: UIImage(named: "AppIcon-in-app")!)
+                            .appIcon(scale: .small)
+                    }
+                )
+            }
+        }
+    }
+}
