afcc96b22e89285d0735c1d274ae0febdc61611a
Milo Wyner <hello@milowyner.com>
2025-10-09 10:42:13 -0700

    Move view extensions and structs in CameraButton.swift to their own files

---
+++ BikeIndex/View/Camera/CameraImagePicker.swift
@@ -0,0 +1,50 @@
+//
+//  CameraImagePicker.swift
+//  BikeIndex
+//
+//  Created by Milo Wyner on 10/9/25.
+//
+
+import SwiftUI
+
+/// A view that represents a UIImagePickerController with `sourceType` set to `.camera`. Used by ``CameraButton`` and ``camera(isPresented:photo:)``
+struct CameraImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) private var presentationMode
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.allowsEditing = true
+        picker.sourceType = .camera
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: CameraImagePicker
+
+        init(_ parent: CameraImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
+        ) {
+            if let image = info[.editedImage] as? UIImage {
+                parent.image = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
