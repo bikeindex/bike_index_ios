1a7a38dabdc55f1f7472aadbf68a050f99dfd37b
Milo Wyner <hello@milowyner.com>
2026-01-16 18:03:05 -0800

    feat: Add many fields to FullBikeRepsponse and Bike, change title to displayTitle, and add unit test

--- BikeIndex/Model/FullBikeResponse.swift
+++ BikeIndex/Model/FullBikeResponse.swift
@@ -49,11 +49,20 @@ struct FullBikeResponse: ResponseModelInstantiable {
 
     /// Rails ID
     let id: Int?
+    let title: String?
     let description: String?
+    
+    let registry_name: String?
+    let registry_url: URL?
+    
     let frame_model: String?
     let frame_colors: [String]
+    let paint_description: String?
+    
     /// Also accepts manufacturer identifier Int
     let manufacturer_name: String
+    let manufacturer_id: Int?
+    
     let year: Int?
     let cycle_type_slug: BicycleType
     let propulsion_type_slug: PropulsionType
@@ -62,18 +71,33 @@ struct FullBikeResponse: ResponseModelInstantiable {
     let serial: String?
 
     let status: BikeStatus
+    let stolen: Bool
     let stolen_coordinates: [CLLocationDegrees]?
     let stolen_location: String?
     let date_stolen: TimeInterval?
+    let location_found: Bool?
 
     let large_img: URL?
     let thumb: URL?
     let url: URL
     let api_url: URL?
     let public_images: [PublicImageResponse]?
+    let is_stock_img: Bool
 
     let registration_created_at: TimeInterval?
     let registration_updated_at: TimeInterval?
+    
+    let extra_registration_number: Int?
+    let rear_tire_narrow: Bool?
+    let test_bike: Bool?
+    let rear_wheel_size_iso_bsd: Bool?
+    let front_wheel_size_iso_bsd: Bool?
+    let handlebar_type_slug: String?
+    let frame_material_slug: String?
+    let front_gear_type_slug: String?
+    let rear_gear_type_slug: String?
+    let additional_registration: String?
+    let components: [String]
 
     // MARK: - ResponseModelInstantiable for BikeResponse
 
@@ -107,28 +131,47 @@ struct FullBikeResponse: ResponseModelInstantiable {
 
         return Bike(
             identifier: id ?? Int.min,
+            title: title,
             bikeDescription: description,
+            registryName: registry_name,
+            registryURL: registry_url,
             frameModel: frame_model,
             primaryColor: firstColor,
             secondaryColor: secondColor,
             tertiaryColor: thirdColor,
+            paintDescription: paint_description,
             manufacturerName: manufacturer_name,
+            manufacturerID: manufacturer_id,
             year: year,
             typeOfCycle: cycle_type_slug,
             typeOfPropulsion: propulsion_type_slug,
             serial: serial,
             status: status,
+            stolen: stolen,
             stolenCoordinateLatitude: stolenCoordinateLatitude,
             stolenCoordinateLongitude: stolenCoordinateLongitude,
             dateStolen: dateStolen,
+            locationFound: location_found,
             largeImage: large_img,
             thumb: thumb,
             url: url,
             apiUrl: api_url,
             publicImages: public_images.simplePublicImages,
             fullPublicImages: public_images.modelInstances(),
+            isStockImage: is_stock_img,
             createdAt: registration_created_at.map { Date(timeIntervalSince1970: $0) },
-            updatedAt: registration_updated_at.map { Date(timeIntervalSince1970: $0) }
+            updatedAt: registration_updated_at.map { Date(timeIntervalSince1970: $0) },
+            extraRegistrationNumber: extra_registration_number,
+            rearTireNarrow: rear_tire_narrow,
+            testBike: test_bike,
+            rearWheelSizeISOBSD: rear_wheel_size_iso_bsd,
+            frontWheelSizeISOBSD: front_wheel_size_iso_bsd,
+            handlebarTypeSlug: handlebar_type_slug,
+            frameMaterialSlug: frame_material_slug,
+            frontGearTypeSlug: front_gear_type_slug,
+            rearGearTypeSlug: rear_gear_type_slug,
+            additionalRegistration: additional_registration,
+            components: components
         )
     }
 }
