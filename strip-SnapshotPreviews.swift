d533fe2676f787e8069c45aef713be43c5104e79
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2026-01-25 19:02:27 -0500

    Project: Update strip-snapshotpreviews.swift script to pass xcodebuild and gym
    - Full command:
    g checkout BikeIndex.xcodeproj/ && swift-sh strip-SnapshotPreviews.swift BikeIndex.xcodeproj && xcodebuild -resolvePackageDependencies && fastlane gym

--- strip-SnapshotPreviews.swift
+++ strip-SnapshotPreviews.swift
@@ -54,26 +54,56 @@ for projects in xcodeproj.pbxproj.projects {
 // MARK: - REMOVE FRAMEWORKS
 let developmentOnlyDependencies = [
     "PreviewGallery", "SnapshotPreferences", // Bike Index
-    "SnapshottingTests" // Unit Tests
+    "SnapshottingTests", // Unit Tests
 ]
 fputs("----------- Build configs \n", stdout)
 for project in xcodeproj.pbxproj.projects {
+    // DONE: Remove SnapshotPreviews package from Bike Index target (solvable)
+    // DONE: Remove SnapshotPreviews package from unit tests target -- BLOCKED because packageProductDependencies does not contain SnapshottingTests, I think XcodeProj is not detecting the Unit Test "Frameworks and Libraries" contents correctly.
+    // TODO: Remove Target.buildPhases that include developmentOnlyDependencies
     for target in project.targets { // PBXTarget
         fputs("\tTARGET A) package deps \(target.name), \(target.packageProductDependencies?.map { $0.productName }) \n", stdout)
         // target.packageProductDependencies?.forEach { printProperties($0) }
         // fputs("\tTARGET B) build config \(target.name), \(target.buildConfigurationList.map(\.buildConfigurations))\n", stdout)
         // fputs("\tTARGET C) deps \(target.name), \(target.dependencies.map(\.target?.name)), \(target.dependencies.map(\.product?.productName))\n", stdout)
-        // TODO: Remove SnapshotPreviews package from Bike Index target (solvable)
-        // TODO: Remove SnapshotPreviews package from unit tests target -- BLOCKED because packageProductDependencies does not contain SnapshottingTests, I think XcodeProj is not detecting the Unit Test "Frameworks and Libraries" contents correctly.
         printProperties(target)
         fputs("\n", stdout)
 
         // 
         if var packageProductDependencies = target.packageProductDependencies {
-            fputs("\(type(of: packageProductDependencies))", stdout)
+            let starting = target.packageProductDependencies?.map { $0.productName }.joined(separator: ", ")
+            fputs("start \(starting)\n", stdout)
             target.packageProductDependencies = packageProductDependencies.filter { 
                 !developmentOnlyDependencies.contains($0.productName) 
             }
+            let ending = target.packageProductDependencies?.map { $0.productName }.joined(separator: ", ")
+            fputs("end \(ending)\n", stdout)
+        }
+
+        let allPhases = target.buildPhases
+        for bPhase in allPhases where bPhase.buildPhase == .frameworks {
+            fputs("\tbPhase.files \(bPhase.files) \n", stdout)
+            fputs("\tbPhase.buildPhase \(bPhase.buildPhase) \n", stdout)
+            fputs("\tbPhase.name \(bPhase.name()) \n", stdout)
+            guard var files = bPhase.files, files.isEmpty == false else {
+                continue
+            }
+            // for file in files {
+            //     file.file.map { fputs("\t\tbPhase.files.file \($0) \n", stdout) }
+            //     fputs("\t\tbPhase.files.product \(file.product), \(file.product?.productName) \n", stdout)
+            //     fputs("\t\tbPhase.files.settings \(file.settings) \n", stdout)
+            //     if let productName = file.product?.productName, developmentOnlyDependencies.contains(productName) {
+            //         files.remove(file)
+            //     }
+            // }
+            files.removeAll { file in
+                if let productName = file.product?.productName, developmentOnlyDependencies.contains(productName) {
+                    true
+                } else {
+                    false
+                }
+            }
+            bPhase.files = files
         }
     }
 }
