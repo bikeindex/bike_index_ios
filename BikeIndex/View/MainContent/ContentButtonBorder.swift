9cd26b7559a8821690a46a402024413a0cef83c0
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2026-01-30 20:06:56 -0500

    Fix bike content advanced gradients to full-width. Next: fix offset/position

--- BikeIndex/View/MainContent/ContentButtonBorder.swift
+++ BikeIndex/View/MainContent/ContentButtonBorder.swift
@@ -10,36 +10,50 @@ import SwiftUI
 @available(*, deprecated, message: "TODO: Rename, this is the wrong name.")
 // ✅ DONE: Use vertical stripes for simplicity and clarity
 // TODO: Add public bike image to the Preview and check with images too
+// TODO: Rely on https://bikeindex.org/api/v3/selections/colors
 struct ContentButtonBorder: View {
     var frameColors: [FrameColor]
 
     var body: some View {
-        HStack {
-            ForEach(.constant(frameColors), id: \.id) { frame in
-
-                switch frame.wrappedValue {
-                // Bare Metal
-                case .bareMetal:
-                    Chip.bareMetalAngularGradient
-                // Covered
-                case .covered:
-                    if #available(iOS 18.0, *) {
-                        Chip.rainbow18
-                    } else {
-                        Chip.rainbow17
-                    }
-                // Color with value
-                case (let value):
-                    if let color = value.color {
-                        // Cannot convert value of type 'Color?' to expected element type 'Array<Color>.ArrayLiteralElement' (aka 'Color')
-                        LinearGradient(colors: [color], startPoint: .leading, endPoint: .trailing)
-                    } else {
-                        Text("Inconsident FrameColor usage")
+        GeometryReader { geo in
+            HStack(spacing: 0) {
+                ForEach(.constant(frameColors), id: \.id) { frame in
+                    switch frame.wrappedValue {
+                    case .bareMetal:
+                        // Bare Metal
+                        Rectangle()
+                            .overlay {
+                                Chip.bareMetalAngularGradient
+                                    .frame(width: geo.size.width, height: geo.size.height)
+                                    .clipped()
+                                    .zIndex(-100)
+                            }
+                    case .covered:
+                        // Covered
+                        if #available(iOS 18.0, *) {
+                            Rectangle()
+                                .overlay {
+                                    Chip.rainbow18
+                                        .frame(width: geo.size.width, height: geo.size.height)
+                                        .onAppear { print("@@", geo.size) }
+                                }
+                                .clipped()
+                        } else {
+                            Chip.rainbow17
+                        }
+                    case (let value):
+                        // Color with value
+                        if let color = value.color {
+                            // TODO: Fix color.gradient here
+                            LinearGradient(colors: [color], startPoint: .leading, endPoint: .trailing)
+                                .zIndex(100)
+                        } else {
+                            Text("Inconsident FrameColor usage")
+                        }
                     }
                 }
             }
         }
-
         .cornerRadius(24)
         .overlay {
             Image(systemName: "bicycle")
@@ -68,10 +82,15 @@ struct ContentButtonBorder: View {
 #Preview {
     ScrollView {
         ProportionalLazyVGrid {
-            ContentButtonBorder(frameColors: [.bareMetal, .blue, .red])
+            ContentButtonBorder(frameColors: [.blue, .red, .bareMetal])
             ContentButtonBorder(frameColors: [.red, .orange, .yellow])
-            ContentButtonBorder(frameColors: [.covered, .white, .black])
+            ContentButtonBorder(frameColors: [.white, .black, .covered])
             ContentButtonBorder(frameColors: [.white])
+            if #available(iOS 18, *) {
+                Chip.rainbow18
+            } else {
+                Chip.rainbow17
+            }
             ContentButtonBorder(frameColors: [.green, .blue, .purple])
         }
     }
