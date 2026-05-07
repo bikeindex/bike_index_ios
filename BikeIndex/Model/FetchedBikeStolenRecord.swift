3cdfa49b1baf669e0de9cd4bcca5aed1a93fa504
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2026-05-06 23:28:53 -0400

    fix: Split FetchedBikeStolenRecord into DTO and @Model (#147)
    - Missed distinction is last big addition
    - Docs:
    https://bikeindex.org/documentation/api_v3#!/bikes/GET_version_bikes_id_format_get_0
    - FetchedBikeStolenRecord was being used both as a DTO in
    FullBikeResponse and stored directly on the Bike @Model. Create a new
    StolenBikeRecord @Model class and have FetchedBikeStolenRecord conform
    to ResponseModelInstantiable with modelInstance() -> StolenBikeRecord,
    matching the existing pattern for ComponentsResponse/Component and
    PublicImageResponse/FullPublicImage.

--- BikeIndex/Model/FetchedBikeStolenRecord.swift
+++ BikeIndex/Model/FetchedBikeStolenRecord.swift
@@ -9,8 +9,10 @@ import CoreLocation
 
 /// Data model to represent a stolen record when fetching a bike.
 /// See https://bikeindex.org/documentation/api_v3#!/bikes/GET_version_bikes_id_format_get_0
-struct FetchedBikeStolenRecord: Codable {
-    let date_stolen: Int?
+struct FetchedBikeStolenRecord: Codable, ResponseModelInstantiable {
+    typealias ModelInstance = StolenBikeRecord
+
+    let date_stolen: TimeInterval?
     let location: String?
     let latitude: CLLocationDegrees?
     let longitude: CLLocationDegrees?
@@ -22,4 +24,21 @@ struct FetchedBikeStolenRecord: Codable {
     let created_at: TimeInterval?
     let create_open311: Bool?
     let id: Int?
+
+    func modelInstance() -> StolenBikeRecord {
+        StolenBikeRecord(
+            dateStolen: date_stolen.map { Date(timeIntervalSince1970: $0) },
+            location: location,
+            latitude: latitude,
+            longitude: longitude,
+            theftDescription: theft_description,
+            lockingDescription: locking_description,
+            lockDefeatDescription: lock_defeat_description,
+            policeReportNumber: police_report_number,
+            policeReportDepartment: police_report_department,
+            createdAt: created_at.map { Date(timeIntervalSince1970: $0) },
+            createOpen311: create_open311,
+            id: id
+        )
+    }
 }
