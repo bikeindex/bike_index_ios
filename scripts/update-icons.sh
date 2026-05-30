97749b6b21c95711556450dcf14ad9b23e299ec5
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2026-05-29 20:34:39 -0400

    Feat: new Pride 2026 icon

--- scripts/update-icons.sh
+++ scripts/update-icons.sh
@@ -14,7 +14,7 @@
 #
 #     ictool input-document.icon --export-preview iOS Light 1024 1024 2 output.png
 #
-alias ictool="/Applications/Xcode_26.app/Contents/Applications/Icon\ Composer.app/Contents/Executables/ictool"
+alias ictool="/Applications/Xcode_26.4.1.app/Contents/Applications/Icon\ Composer.app/Contents/Executables/ictool"
 
 # create an array of all source Icon Composer files
 icon_composer_files=(BikeIndex/AppIcons/*.icon)
@@ -33,4 +33,4 @@ for ((i=0; i<${#icon_composer_files[@]}; i++)); do
 done
 
 xed BikeIndex.xcodeproj
-open BikeIndex/Assets.xcassets -a /Applications/Xcode_26.app
+open BikeIndex/Assets.xcassets -a /Applications/Xcode_26.4.1.app
